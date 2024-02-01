2d8e8c92cc54db914e6c7a1367a30b449d52e272
Akihiro Yamada <153415348+mountainpaddy040818@users.noreply.github.com>
2024-02-01 15:15:56 +0000

    20240202 00:15

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,11 @@ Rails.application.routes.draw do
     resource :favorite, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update] do 
+  resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
     get "followings" => "relationships#followings", as: "followings"
     get "followers" => "relationships#followers", as: "followers"
-  end 
-  get "search" => "searches#search"
+  end
+  get "/search", to: "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
